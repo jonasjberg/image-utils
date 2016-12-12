7259440e8d1aefcdc7e3eebc56065ce0f52fa32e
Jonas Sjöberg <jomeganas@gmail.com>
2016-12-12 14:56:34 +0100

    Move main routine to separate function.

--- extract-android-thumbdata.py
+++ extract-android-thumbdata.py
@@ -3,26 +3,33 @@
 # http://android.stackexchange.com/questions/58087/read-content-of-thumbdata-file
 # http://android.stackexchange.com/a/109739
 #
-"""extract files from Android thumbdata3 file"""
-
-f = open('thumbdata3.dat', 'rb')
-tdata = f.read()
-f.close()
 
 JPEG_FILE_START = '\xff\xd8'
 JPEG_FILE_END = '\xff\xd9'
 
-count = 0
-start = 0
-while True:
-    x1 = tdata.find(JPEG_FILE_START, start)
-    if x1 < 0:
-        break
-    x2 = tdata.find(JPEG_FILE_END, x1)
-    jpg = tdata[x1:x2 + 1]
-    count += 1
-    fname = 'extracted%d03.jpg' % (count)
-    fw = open(fname, 'wb')
-    fw.write(jpg)
-    fw.close()
-    start = x2 + 2
+
+def extract_files_from_thumbdata_file(path):
+    """extract files from Android thumbdata3 file"""
+
+    f = open(path, 'rb')
+    tdata = f.read()
+    f.close()
+
+    count = 0
+    start = 0
+    while True:
+        x1 = tdata.find(JPEG_FILE_START, start)
+        if x1 < 0:
+            break
+        x2 = tdata.find(JPEG_FILE_END, x1)
+        jpg = tdata[x1:x2 + 1]
+        count += 1
+        fname = 'extracted%d03.jpg' % (count)
+        fw = open(fname, 'wb')
+        fw.write(jpg)
+        fw.close()
+        start = x2 + 2
+
+
+if __name__ == '__main__':
+    extract_files_from_thumbdata_file('thumbdata3.dat')
