5358cb0aa21d3b594eb93c73807589472f510269
Jonas Sjöberg <jomeganas@gmail.com>
2016-12-13 11:47:05 +0100

    Various fixes. Good enough for now.

--- extract-android-thumbdata.py
+++ extract-android-thumbdata.py
@@ -5,50 +5,54 @@
 #
 
 import os
+import sys
 
-JPEG_FILE_START = '\xff\xd8'
-JPEG_FILE_END = '\xff\xd9'
+JPEG_HEADER_START = '\xff\xd8'
+JPEG_HEADER_END = '\xff\xd9'
 
 
 def extract_files_from_thumbdata_file(path):
     """extract files from Android thumbdata3 file"""
 
-    f = open(path, 'rb')
-    tdata = f.read()
-    f.close()
+    with open(path, 'rb') as f:
+        thumb_data = f.read()
 
     count = 0
     start = 0
     while True:
-        x1 = tdata.find(JPEG_FILE_START, start)
+        x1 = thumb_data.find(JPEG_HEADER_START, start)
         if x1 < 0:
             break
-        x2 = tdata.find(JPEG_FILE_END, x1)
+        x2 = thumb_data.find(JPEG_HEADER_END, x1)
+        jpg = thumb_data[x1:x2 + 1]
 
-        jpg = tdata[x1:x2 + 1]
-
-        out_file = 'extracted%d03.jpg' % (count)
-        fw = open(out_file, 'wb')
-        fw.write(jpg)
-        fw.close()
+        out_file = 'extracted{:03d}.jpg'.format(count)
+        with open(out_file, 'wb') as fw:
+            fw.write(jpg)
 
         start = x2 + 2
         count += 1
+
+    print('Wrote {} files'.format(count))
 
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         print('Usage: {} [FILE [FILE ...]]'.format(sys.argv[0]))
-        sys.exit(0)
+        print('')
+        print(' NOTE: Any files named "extracted%03d.jpg" in working ')
+        print('       directory could be overwritten! Use with caution.')
+        sys.exit(1)
     else:
-        for arg in sys.argv:
-            if not os.path.isfile(arg):
-                print('Not a file: "{}"'.format(str(arg)))
-                continue
-            else:
+        for arg in sys.argv[1:]:
+            if os.path.isfile(arg):
                 if not os.access(arg, os.R_OK):
                     print('Not authorized to read file: "{}"'.format(str(arg)))
                     continue
                 else:
-                    extract_files_from_thumbdata_file('thumbdata3.dat')
+                    extract_files_from_thumbdata_file(arg)
+            else:
+                print('Not a file: "{}"'.format(str(arg)))
+                continue
 
+        sys.exit(0)
