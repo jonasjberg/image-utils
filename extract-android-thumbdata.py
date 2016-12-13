9571020a9cbd36780575a23e0470699f22a2c86b
Jonas Sjöberg <jomeganas@gmail.com>
2016-12-13 11:18:12 +0100

    Add handling of input arguments. Fixes.

--- extract-android-thumbdata.py
+++ extract-android-thumbdata.py
@@ -4,6 +4,8 @@
 # http://android.stackexchange.com/a/109739
 #
 
+import os
+
 JPEG_FILE_START = '\xff\xd8'
 JPEG_FILE_END = '\xff\xd9'
 
@@ -22,14 +24,31 @@ def extract_files_from_thumbdata_file(path):
         if x1 < 0:
             break
         x2 = tdata.find(JPEG_FILE_END, x1)
+
         jpg = tdata[x1:x2 + 1]
-        count += 1
-        fname = 'extracted%d03.jpg' % (count)
-        fw = open(fname, 'wb')
+
+        out_file = 'extracted%d03.jpg' % (count)
+        fw = open(out_file, 'wb')
         fw.write(jpg)
         fw.close()
+
         start = x2 + 2
+        count += 1
 
 
 if __name__ == '__main__':
-    extract_files_from_thumbdata_file('thumbdata3.dat')
+    if len(sys.argv) < 2:
+        print('Usage: {} [FILE [FILE ...]]'.format(sys.argv[0]))
+        sys.exit(0)
+    else:
+        for arg in sys.argv:
+            if not os.path.isfile(arg):
+                print('Not a file: "{}"'.format(str(arg)))
+                continue
+            else:
+                if not os.access(arg, os.R_OK):
+                    print('Not authorized to read file: "{}"'.format(str(arg)))
+                    continue
+                else:
+                    extract_files_from_thumbdata_file('thumbdata3.dat')
+
